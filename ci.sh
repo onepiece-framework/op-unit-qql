598361db5c47f79be39210c125674759a6fb0354
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-01-28 10:22:56 +0900

    New: ci.sh

---
+++ ci.sh
@@ -0,0 +1,12 @@
+
+ ## op-unit-empty:/ci.sh
+ #
+ # Call from git pre-push
+ #
+ # @created   2022-11-14
+ # @version   1.0
+ # @package   op-unit-empty
+ # @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright Tomoaki Nagahara All right reserved.
+
+php ../../../ci.php $@
