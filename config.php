1e4039dca65f86fb2945f3684ba997549bae5bc2
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-01-30 17:15:38 +0900

    New: config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/** op-unit-empty:/config.php
+ *
+ * Default config file.
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-empty
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+//	...
+return [
+];
