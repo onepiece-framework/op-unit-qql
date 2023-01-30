3e44c070eacc7be34b357662f740a49f8514bbf1
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-01-30 17:15:20 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/** op-unit-empty:/index.php
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-empty
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Include
+ *
+ */
+include(__DIR__.'/Empty.class.php');
