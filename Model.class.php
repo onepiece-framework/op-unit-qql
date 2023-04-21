82190df3bf0d9dc1f39e6b1c48eba9a86f5ce620
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-04-21 21:31:17 +0900

    Fix: Empty --> Model

--- Model.class.php
+++ Model.class.php
@@ -1,9 +1,9 @@
 <?php
-/** op-unit-empty:/Empty.class.php
+/** op-unit-model:/Model.class.php
  *
  * @created    2023-01-30
  * @version    1.0
- * @package    op-unit-empty
+ * @package    op-unit-model
  * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright  Tomoaki Nagahara All right reserved.
  */
@@ -25,15 +25,15 @@ use OP\IF_UNIT;
 use OP\OP_CORE;
 use OP\OP_CI;
 
-/** Empty
+/** Model
  *
  * @created    2023-01-30
  * @version    1.0
- * @package    op-unit-empty
+ * @package    op-unit-model
  * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright  Tomoaki Nagahara All right reserved.
  */
-class _Empty implements IF_UNIT
+class Model implements IF_UNIT
 {
 	use OP_CORE, OP_CI;
 }
