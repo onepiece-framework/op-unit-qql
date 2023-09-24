37c390023624d92c1f6b297d1f38c2c46b002f02
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-09-24 17:45:46 +0900

    Fix: index.php | PHPDoc

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /** op-unit-model:/index.php
  *
- * @created    2023-01-30
- * @version    1.0
- * @package    op-unit-model
- * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright  Tomoaki Nagahara All right reserved.
+ * @created     2023-01-30
+ * @version     1.0
+ * @package     op-unit-model
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
  */
 
- /** Declare strict
+/** Declare strict
  *
  */
 declare(strict_types=1);
