d66467cf1c824c854e769811d627452af04cd42c
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-09-24 17:46:09 +0900

    Fix: Model.class.php | PHPDoc

--- Model.class.php
+++ Model.class.php
@@ -1,11 +1,11 @@
 <?php
 /** op-unit-model:/Model.class.php
  *
- * @created    2023-01-30
- * @version    1.0
- * @package    op-unit-model
- * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright  Tomoaki Nagahara All right reserved.
+ * @created     2023-01-30
+ * @version     1.0
+ * @package     op-unit-model
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
  */
 
  /** Declare strict
@@ -27,11 +27,11 @@ use OP\OP_CI;
 
 /** Model
  *
- * @created    2023-01-30
- * @version    1.0
- * @package    op-unit-model
- * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright  Tomoaki Nagahara All right reserved.
+ * @created     2023-01-30
+ * @version     1.0
+ * @package     op-unit-model
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
  */
 class Model implements IF_UNIT
 {
