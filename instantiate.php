5972c2ff40909ea307fe0402c57dec9f3c531b2e
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-03-19 01:44:39 +0900

    New: instantiate.php | Return instanced object.

---
+++ instantiate.php
@@ -0,0 +1,29 @@
+<?php
+/** op-unit-model:/instantiate.php
+ *
+ * @created     2024-03-19
+ * @version     1.0
+ * @package     op-unit-model
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT;
+
+/** Include
+ *
+ */
+require_once(__DIR__.'/Model.class.php');
+
+/** Return instance
+ *
+ */
+return new \OP\UNIT\Model();
