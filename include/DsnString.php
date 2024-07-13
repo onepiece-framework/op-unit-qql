995c3b03214205b60f33b88129faee8da1f8ca98
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-07-13 18:49:29 +0900

    New: QQL :: Open()

---
+++ include/DsnString.php
@@ -0,0 +1,33 @@
+<?php
+/** op-unit-qql:/include/DsnString.php
+ *
+ * @created    2024-07-15
+ * @version    1.0
+ * @package    op-unit-qql
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\QQL;
+
+/* @var $dsn string */
+
+//	...
+$root = OP()->MetaPath('asset:/db/');
+$path = $root . $dsn;
+
+//	...
+if(!file_exists($path) ){
+	throw new \Exception("Database file does not exists. `$path`");
+}
+
+//	...
+return 'sqlite:' . $path;
