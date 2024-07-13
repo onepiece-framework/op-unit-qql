dacf984351030b0faec2518ee0241b12e2aeb43f
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-07-13 22:45:06 +0900

    New: QQL :: Insert(), include > fields.php, values.php, sandbox > Insert.php

---
+++ include/table.php
@@ -0,0 +1,25 @@
+<?php
+/** op-unit-qql:/include/table.php
+ *
+ * @created    2024-07-13
+ * @version    1.0
+ * @package    op-unit-qql
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\QQL;
+
+/* @var $quote string */
+/* @var $table string */
+
+//	...
+return "{$quote}{$table}{$quote}";
