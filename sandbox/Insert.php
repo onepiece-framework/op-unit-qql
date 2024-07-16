8f9b27607701165de07a3f1fe5b43f36ca73b9c3
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-07-17 03:35:39 +0900

    Fix: sandbox > Insert.php, Update.php, Select.php

--- sandbox/Insert.php
+++ sandbox/Insert.php
@@ -19,20 +19,37 @@ declare(strict_types=1);
 namespace OP\UNIT\QQL;
 
 //	...
+echo ' [ <a href="?table=t_user" >t_user</a>';
+echo ' | <a href="?table=t_group">t_group</a> ] ';
+
+//	...
 if(!$table = OP()->Request('table') ){
 	D('table is empty.');
 	return;
 }
 
 //	...
-if(!$name = OP()->Request('name') ){
-	D('name is empty.');
+if( $name = OP()->Request('name') ){
+	$set['name'] = $name;
+}
+if( $group = OP()->Request('group') ){
+	$set['group'] = $group;
+}
+if( $age = OP()->Request('age') ){
+	$set['age'] = $age;
+}
+
+//	...
+if( empty($set) ){
+	include_once('Open.php');
+	D( OP()->Unit()->QQL()->Get(" $table ") );
+	OP()->Html('Update value is empty. ex: name, group, age');
 	return;
 }
 
 //	...
-include('Open.php');
+include_once('Open.php');
 
 //	...
-$ai = OP()->Unit()->QQL()->Set($table, ['name'=>$name]);
-D($ai);
+D( OP()->Unit()->QQL()->Set($table, ['name'=>$name]) );
+D( OP()->Unit()->QQL()->Get($table, [], ['limit'=>-1]) );
--- sandbox/Update.php
+++ sandbox/Update.php
@@ -19,30 +19,42 @@ declare(strict_types=1);
 namespace OP\UNIT\QQL;
 
 //	...
+echo ' [ <a href="?table=t_user" >t_user</a>';
+echo ' | <a href="?table=t_group">t_group</a> ] ';
+
+//	...
+include_once('Open.php');
+
+//	...
 if(!$table = OP()->Request('table') ){
-	D('table is empty.');
+	OP()->Html('table is empty.');
 	return;
 }
 
 //	...
 if(!$ai = (int)OP()->Request('ai') ){
-	D('ai is empty.');
+	OP()->Html('ai is empty.');
+	D( OP()->Unit()->QQL()->Get(" $table ", [], ['limit'=>-1]) );
 	return;
 }
 
 //	...
-if(!$name = OP()->Request('name') ){
-	D('name is empty.');
-	return;
+if( $name = OP()->Request('name') ){
+	$set['name'] = $name;
+}
+if( $group = OP()->Request('group') ){
+	$set['group'] = $group;
+}
+if( $age = OP()->Request('age') ){
+	$set['age'] = $age;
 }
 
 //	...
-include('Open.php');
-
-//	...
-$set = [
-	'name' => $name,
-];
+if( empty($set) ){
+	D( OP()->Unit()->QQL()->Get(" $table ") );
+	OP()->Html('Update value is empty. ex: name, group, age');
+	return;
+}
 
 //	...
 $where = [
@@ -50,5 +62,5 @@ $where = [
 ];
 
 //	...
-$ai = OP()->Unit()->QQL()->Set($table, $set, $where);
-D($ai);
+D( OP()->Unit()->QQL()->Set($table, $set, $where) );
+D( OP()->Unit()->QQL()->Get(" {$table}.ai = {$ai} ", ) );
