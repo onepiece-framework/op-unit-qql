0eda5b42e84f98285dd93607331dca49878425e7
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-07-23 02:07:32 +0900

    Fix: include > option.php | GROUP, ORDER

--- include/option.php
+++ include/option.php
@@ -23,31 +23,54 @@ namespace OP\UNIT\QQL;
 //	...
 $OPTION = [];
 
-//	...
+//	Convert to assoc from strings array.
+$temp = [];
 foreach( $option as $key => $val ){
-
 	//	...
 	if( is_integer($key) ){
 		$val = trim($val);
 		if( $pos = strpos($val, ' ') ){
-			$key = substr($val, 0, $pos);
-			$val = substr($val, $pos +1);
+			$key = trim( substr($val, 0, $pos) );
+			$val = trim( substr($val, $pos +1) );
 		}else{
 			D('Does not match option format.', $val);
 			continue;
 		}
 	}
+	//	...
+	$temp[$key] = $val;
+}
+$option = $temp;
+
+//	...
+foreach( ['group','order','limit','offset','page',] as $key ){
+	//	...
+	if(!$val = $option[$key] ?? null ){
+		continue;
+	}
 
 	//	...
 	switch( $key = strtoupper($key) ){
-		case 'LIMIT':
-			if( $val > 0 ){
-				$OPTION[] = $key.' '.$val;
-			}
+		case 'GROUP':
+			$val = self::Quote($val);
+			$OPTION[] = $key.' BY '.$val;
 			break;
 
 		case 'ORDER':
+			if( $pos = strrpos($val, ' desc') ){
+				$val = substr($val, 0, $pos);
+				$val = self::Quote($val) . ' DESC';
+			}else{
+				$val = self::Quote($val);
+			}
 			$OPTION[] = $key.' BY '.$val;
+			break;
+
+		case 'LIMIT':
+		case 'OFFSET':
+			if( is_int($val) and $val > 0 ){
+				$OPTION[] = $key.' '.$val;
+			}
 			break;
 	}
 }
