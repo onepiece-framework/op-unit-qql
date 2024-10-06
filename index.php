52ae0cabdfe5cb9170cea9b6deed0cb3be25e274
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-10-07 03:33:27 +0900

    Fix: index.php | If is CI automatically copy of QQL.sqlite3 file.

--- index.php
+++ index.php
@@ -22,3 +22,14 @@ namespace OP;
  *
  */
 require_once(__DIR__.'/QQL.class.php');
+
+//	For CI
+if( Env::isCI() ){
+	//	...
+	$file = 'QQL.sqlite3';
+	$path = OP()->MetaPath("asset:/db/ci/{$file}");
+	//	...
+	if(!file_exists( $path ) ){
+		copy(__DIR__.'/'.$file, $path);
+	}
+}
